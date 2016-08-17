7ad11279c7deb188da529a644ae1f9bf05d7f5ca
Jefri Vanegas <vjefri@gmail.com>
2016-08-17 04:09:39 -0700

    fix proxy

--- proxy.js
+++ proxy.js
@@ -1,14 +1,15 @@
 const httpProxy = require('http-proxy');
 const url = 'http://spa.tglrw.com:4000';
+const https = require('https');
 
 module.exports = function () {
   console.log('proxy on port 8001');
-  const options = {
-    changeOrigin: true,
-    target: {
-      https: true
-    }
-  };
 
-  httpProxy.createServer(443, url, options).listen(8001);
+  httpProxy.createProxyServer({
+    target: 'https://spa.tglrw.com:4000',
+    agent: https.globalAgent,
+    headers: {
+      host: 'spa.tglrw.com'
+    }
+  }).listen(8001);
 };
